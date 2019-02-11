166d767e69f6599f59fdc3b99a692b738ac856a2
lpq169128 <542245193@qq.com>
2019-02-11 15:19:45 +0800

    提交测试数据

---
+++ timss-pf/timss-pf/src/main/java/com/timss/pf/web/testController.java
@@ -0,0 +1,14 @@
+package com.timss.pf.web;
+
+/**
+ * @ Author     ：LiPanQing
+ * @ Date       ：Created in 2019/2/11/011
+ * @ Description：${description}
+ * @ Modified By：
+ */
+public class testController {
+
+    public static void main(String[] args) {
+        System.out.println("测试提交数据");
+    }
+}
